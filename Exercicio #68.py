7561b04552087f2addf588f1f8ba5662d7a6989f
Ryan Jesus Valentim de Oliveira <ryan.jesus@hotmail.com>
2022-07-07 16:27:29 -0300

    Exercicios de python

--- Exercicio #68.py
+++ Exercicio #68.py
@@ -4,11 +4,28 @@ O jogo só será interrompido quando o jogador perde,
 mostrando o total de vitórias consecutivas que ele conquistou no
 final do jogo.
 """
-
+from random import randint
+v = 0
 while True:
     valor_usuario = int(input('Digite um valor: '))
-    escolha = str(input('Escolha entre Par ou Impar [P/I]: ')).upper()
-    if valor_usuario % 2 == 0:
-        print('Você Escolheu' if escolha == 'I' 'Impar' else 'Par')
-    elif valor_usuario % 2 != 0 and escolha == 'I':
-        print('Você Escolheu' if escolha == 'I' 'Impar' else 'Par')
+    computador = randint(0, 10)
+    total = valor_usuario + computador
+    tipo = ' '
+    while tipo not in 'PpIi':
+        tipo = str(input('Par ou Impar? ')).strip().upper()[0]
+    print(f'Você jogou {valor_usuario} e o computador {computador}. total de {total}')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu')
+    print('Vamos jogar novamente...')
+print(f'Game over! Você venceu {v} vezes')
---
+++ Exercicio #69.py
@@ -0,0 +1,42 @@
+"""
+Crie um programa que leia a idade e o sexo de várias peassos. A cada pessoa
+cadastrada, o programa deverá pergunta se o usuário quer ou não continuar.
+
+No final, mostre:
+
+a) Quantas pessoas tem mais de 18 anos.
+b) Quantos homens foram cadastrados.
+c) Quantas mulheres tem menos de 20 anos
+"""
+
+
+contidade = 0
+contisexom = 0
+contisexof = 0
+
+while True:
+    print('-=-' * 20)
+    print('CADASTRE UMA PESSOA')
+    print('-=-' * 20)
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    q = ' '
+    while q not in 'SN':
+        q = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if idade > 18:
+        contidade += 1
+    if sexo == 'F' and idade < 20:
+        contisexof += 1
+    if sexo == 'M':
+        contisexom += 1
+    if q == 'N':
+        print('Saindo do programa! ')
+        break
+print('-=-' * 20)
+print(f'{contidade} são maiores de 18 anos')
+print('-=-' * 20)
+print(f'{contisexom} são homens')
+print('-=-' * 20)
+print(f'{contisexof} mulheres são menor de 20 anos')
