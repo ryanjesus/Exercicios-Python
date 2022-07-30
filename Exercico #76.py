981248500409d4fc9d27dc696e47199a2fab7d37
Ryan Jesus Valentim de Oliveira <ryan.jesus@hotmail.com>
2022-07-30 14:06:30 -0300

    Exercicios de python

--- Exercico #76.py
+++ Exercico #76.py
@@ -7,10 +7,13 @@ No final, mostre uma listagem de preços, organizado os dados em forma tabular
 
 c = 0
 listagem = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
-
+pontinhos = 0
+pontos = ''
 while True:
     for item in listagem:
-        print(f'\n{listagem[c]}.............. R$' if c % 2 == 0 else listagem[c], end=' ')
+        if c % 2 == 0:
+            pontinhos = (44 - len(item))
+            pontos = '.' * pontinhos
+        print(f'\n{listagem[c]} {pontos} R$' if c % 2 == 0 else f'{listagem[c]:.2f}', end=' ')
         c += 1
     break
-
