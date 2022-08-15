3a7fd199a68d6871868f7e898d2c12212f8474b5
Ryan Jesus Valentim de Oliveira <ryan.jesus@hotmail.com>
2022-08-14 22:37:17 -0300

    Exercicios de python

---
+++ Exercicio #100.py
@@ -0,0 +1,17 @@
+"""
+Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar().
+A primeira função vai sortear 5 números e vai coloca-los dentros da lista e a segunda função vai
+mostrar a soma entre todos os valores PARES sorteados pela função anterior
+"""
+
+numeros = list()
+from random import randint
+
+def sorteia():
+    for c in range(0, 6):
+        randint(0, 100)
+
+
+
+numeros.append(sorteia())
+print(numeros)
